8192e58d0197227ec6fb0b154f65930954237d85
zOomxig <907634353@qq.com>
2022-07-22 10:12:41 +0800

    modified

--- api/data.js
+++ api/data.js
@@ -11,5 +11,14 @@ export const getMenu = (param) => {
 export const getData = () => {
     return axios.request({
         url: "/home/getData",
+        method: 'get'
+    })
+}
+
+export const getUser = (params) => {
+    return axios.request({
+        url: '/user/getUser',
+        method: 'get',
+        params
     })
 }
